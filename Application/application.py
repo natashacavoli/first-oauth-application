3a6eca889bafcea36960a2cc7404f8553171a330
natashacavoli <natashacavoli@hotmail.com>
2020-05-22 16:44:01 -0300

    Adding files of application and API.

---
+++ Application/application.py
@@ -0,0 +1,14 @@
+"""."""
+
+
+class User():
+    """."""
+
+    def __init__(self):
+        """."""
+
+    def get_user(self):
+        """."""
+        data = {"name": "name"}
+
+        return data
